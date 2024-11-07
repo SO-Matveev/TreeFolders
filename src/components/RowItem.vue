889f2be3256ff4c1c9eef21a807158d28aab2f7f
s.matveev <matveev.stanislav@yahoo.com>
2024-11-07 20:50:57 +0300

    корректировка типа, отрисовка дерева через рекурсию, проверка иконок.

--- src/components/RowItem.vue
+++ src/components/RowItem.vue
@@ -1,53 +1,85 @@
 <script lang="ts">
-  import type Itree from "@/stores/model";
+import type Itree from "@/stores/model";
 
-  export default {
-    props: {
-      treeFolders: Array<Itree>,
-      depth: {
-        type: Number,
-        default: 0,
-      }
-    },
-    data() {
-      return {
-        parent: false,
-        child: false,
-
-      }
+export default {
+  name: "RowItem",
+  props: {
+    treeFolders: Array<Itree>,
+    depth: {
+      type: Number,
+      default: 0,
+    }
+  },
+  data() {
+    return {
+      parent: false,
+      child: false,
     }
   }
+}
 </script>
 
 <template>
-      <ul
-        class="folder"
+
+  <ul>
+    <div class="container">
+      <li
+        class="item"
         v-for="treeFolder in treeFolders"
         :key="treeFolder.name"
       >
         <span @click="parent = !parent">{{ treeFolder.name }} </span>
-        <li class="file" @click="child = !child" v-for="folder in treeFolder.children">
-          <span v-if="!!parent"  >{{ folder.name }}</span>
-          <li class="file" v-for="file in folder.children">
-            <span v-if="!!parent && child">{{ file.name }}</span>
-          </li>
-        </li>
-      </ul>
+        <RowItem :treeFolders="treeFolder.children" v-if="treeFolder.children"/>
+      </li>
+    </div>
+  </ul>
+
+  <!--      <ul-->
+  <!--        class="row"-->
+  <!--        v-for="treeFolder in treeFolders"-->
+  <!--        :key="treeFolder.name"-->
+  <!--      >-->
+  <!--          <div>-->
+  <!--            <fa icon="folder" v-if="treeFolder.name.includes('Dir')"></fa>-->
+  <!--            <fa icon="file" v-if="treeFolder.name.includes('File')"></fa>-->
+  <!--            <span @click="parent = !parent">{{ treeFolder.name }} </span>-->
+  <!--            <fa icon="pen-to-square"/>-->
+  <!--            <fa icon="xmark"></fa>-->
+  <!--          </div>-->
+
+  <!--        <li class="row" @click="child = !child" v-for="folder in treeFolder.children">-->
+  <!--          <div v-if="!!parent">-->
+  <!--            <fa icon="folder" v-if="folder.name.includes('Dir')"></fa>-->
+  <!--            <fa icon="file" v-if="folder.name.includes('File')"></fa>-->
+  <!--            <span>{{ folder.name }}</span>-->
+  <!--            <fa icon="pen-to-square"/>-->
+  <!--            <fa icon="xmark"></fa>-->
+  <!--          </div>-->
+
+  <!--          <li class="row" v-for="file in folder.children">-->
+  <!--            <div v-if="!!parent && child">-->
+  <!--              <fa icon="folder" v-if="file.name.includes('Dir')"></fa>-->
+  <!--              <fa icon="file" v-if="file.name.includes('File')"></fa>-->
+  <!--              <span >{{ file.name }}</span>-->
+  <!--              <fa icon="pen-to-square"/>-->
+  <!--              <fa icon="xmark"></fa>-->
+  <!--            </div>-->
+  <!--          </li>-->
+  <!--        </li>-->
+  <!--      </ul>-->
+
 </template>
 
 <style scoped>
-.folder{
+.row {
+  margin-left: 3rem;
+  list-style: none;
+  padding-left: 0.5rem;
+  padding-right: 1rem;
+
   :hover {
     background-color: rgba(170, 161, 161, 0.4);
     color: #000000
   }
 }
-      .file {
-        margin-left: 3rem;
-        list-style: none;
-        :hover {
-          background-color: rgba(170, 161, 161, 0.4);
-          color: #000000
-        }
-      }
 </style>
