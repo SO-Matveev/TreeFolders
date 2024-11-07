900ff86731ba93ca48c06936384c5c55423eeec8
s.matveev <matveev.stanislav@yahoo.com>
2024-11-07 20:49:21 +0300

    установка иконок fontAwesome

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+library.add(fas)
 
-app.use(createPinia())
-app.use(router)
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .component('fa', FontAwesomeIcon)
+  .mount('#app')
 
-app.mount('#app')
+
+
