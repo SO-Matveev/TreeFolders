710a5b50846be885013a6a291888babdcf84d296
s.matveev <matveev.stanislav@yahoo.com>
2024-11-07 18:44:30 +0300

    логика разворачивания папок

--- src/components/RowItem.vue
+++ src/components/RowItem.vue
@@ -1,26 +1,53 @@
 <script lang="ts">
+  import type Itree from "@/stores/model";
 
   export default {
     props: {
-      treeFolders: {
-        required: true,
+      treeFolders: Array<Itree>,
+      depth: {
+        type: Number,
+        default: 0,
+      }
+    },
+    data() {
+      return {
+        parent: false,
+        child: false,
+
       }
     }
   }
-
 </script>
 
 <template>
-      <ul class="folder" v-for="treeFolder in treeFolders">
-        <span>{{ treeFolder.name }}</span>
-          <li class="file">
-            <span>{{ treeFolder.children }}</span>
+      <ul
+        class="folder"
+        v-for="treeFolder in treeFolders"
+        :key="treeFolder.name"
+      >
+        <span @click="parent = !parent">{{ treeFolder.name }} </span>
+        <li class="file" @click="child = !child" v-for="folder in treeFolder.children">
+          <span v-if="!!parent"  >{{ folder.name }}</span>
+          <li class="file" v-for="file in folder.children">
+            <span v-if="!!parent && child">{{ file.name }}</span>
           </li>
+        </li>
       </ul>
 </template>
 
 <style scoped>
+.folder{
+  :hover {
+    background-color: rgba(170, 161, 161, 0.4);
+    color: #000000
+  }
+}
       .file {
         margin-left: 3rem;
+        list-style: none;
+        :hover {
+          background-color: rgba(170, 161, 161, 0.4);
+          color: #000000
+        }
       }
 </style>
